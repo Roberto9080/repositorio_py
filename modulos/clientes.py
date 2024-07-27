16164896280c7b1dba4d4528a4af679c90d28cf3
Roberto9080 <bechaenel@gmail.com>
2024-07-27 16:09:54 -0600

    actualizado el apartado de clientes

--- modulos/clientes.py
+++ modulos/clientes.py
@@ -62,3 +62,65 @@ def add_cliente():
         return redirect(url_for('clientes.add_cliente'))
 
     return render_template('add_cliente.html')
+
+# Ruta para ver clientes
+@clientes_bp.route('/see_clientes', methods=['GET', 'POST'])
+def see_clientes():
+    if verificar_sesion():
+        return verificar_sesion()
+
+    search_query = ''
+    filters = {'nombre': '', 'apellido': '', 'domicilio': '', 'telefono': ''}
+    order = request.args.get('order', None)
+
+    if request.method == 'POST':
+        search_query = request.form.get('search', '')
+        filters['nombre'] = request.form.get('nombre', '')
+        filters['apellido'] = request.form.get('apellido', '')
+        filters['domicilio'] = request.form.get('domicilio', '')
+        filters['telefono'] = request.form.get('telefono', '')
+
+    query = "SELECT * FROM Cliente WHERE 1=1"
+    params = []
+
+    if search_query:
+        query += " AND (Nombre LIKE %s OR Apellido LIKE %s OR Domicilio LIKE %s OR Telefono LIKE %s)"
+        search_pattern = f"%{search_query}%"
+        params.extend([search_pattern, search_pattern, search_pattern, search_pattern])
+
+    if filters['nombre']:
+        query += " AND Nombre LIKE %s"
+        params.append(f"%{filters['nombre']}%")
+    if filters['apellido']:
+        query += " AND Apellido LIKE %s"
+        params.append(f"%{filters['apellido']}%")
+    if filters['domicilio']:
+        query += " AND Domicilio LIKE %s"
+        params.append(f"%{filters['domicilio']}%")
+    if filters['telefono']:
+        query += " AND Telefono LIKE %s"
+        params.append(f"%{filters['telefono']}%")
+
+    if order:
+        if order == 'nombre_asc':
+            query += " ORDER BY Nombre ASC"
+        elif order == 'nombre_desc':
+            query += " ORDER BY Nombre DESC"
+        elif order == 'apellido_asc':
+            query += " ORDER BY Apellido ASC"
+        elif order == 'apellido_desc':
+            query += " ORDER BY Apellido DESC"
+        elif order == 'domicilio_asc':
+            query += " ORDER BY Domicilio ASC"
+        elif order == 'domicilio_desc':
+            query += " ORDER BY Domicilio DESC"
+
+    conexion = obtener_conexion()
+    cursor = conexion.cursor(dictionary=True)
+    cursor.execute(query, params)
+    clientes = cursor.fetchall()
+    cursor.close()
+    conexion.close()
+
+    return render_template('see_clientes.html', clientes=clientes, search_query=search_query, filters=filters)
+
